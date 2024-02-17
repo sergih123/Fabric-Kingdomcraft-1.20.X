2caaa4305c82d4b21cc5a9565cc1065dd925c577
sergih123 <56600431+sergih123@users.noreply.github.com>
2024-02-17 19:24:02 +0100

    Initial commit

---
+++ src/main/java/net/sergih/kingdomcraft/KingdomCraft.java
@@ -0,0 +1,17 @@
+package net.sergih.kingdomcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KingdomCraft implements ModInitializer {
+	public static final String MOD_ID = "kingdomcraft";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/sergih/kingdomcraft/KingdomCraftClient.java
@@ -0,0 +1,10 @@
+package net.sergih.kingdomcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KingdomCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
